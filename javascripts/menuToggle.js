9a012b3fe0fd28a2770fdf4642d54c515cd476ba
Hiromi Hoshi <hoshi@sackle.co.jp>
2017-04-12 18:38:48 +0900

    Site updated at 2017-04-12 09:38:48 UTC

---
+++ javascripts/menuToggle.js
@@ -0,0 +1,18 @@
+$220(function() {
+  $220("#toggle").click(function() {
+   $220(".main-navigation").slideToggle();
+    return false;
+  });
+  
+//  $220(window).resize(function() {
+//   var win = $(window).width();
+//   var p = 550;
+//   if(win > p) {
+//    $220(".main-navigation").show();
+//   }
+//   else {
+//    $220(".main-navigation").hide();
+//   }
+// });
+
+});
